31213d9186a16c8ca5e17fd7440974882f7b1437
user <user@DESKTOP-PNR9EFR>
2026-02-28 10:48:38 -0500

    Add Preliminar Workshop POO01

---
+++ WorkshopPOO-01/WorkshopNumer01ofPOO/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace WorkshopNumer01ofPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Time t1 = new Time(23, 58, 34, 666);
+                Time t2 = new Time(12, 0, 0, 0);
+                Time t3 = new Time(5, 30, 0, 0);
+                Time t4 = new Time(0, 0, 0, 1);
+                Time t5 = new Time(15, 45, 23, 456);
+
+                Time[] times = { t1, t2, t3, t4, t5 };
+
+                Console.WriteLine("HORAS CREADAS:");
+                foreach (Time t in times)
+                {
+                    Console.WriteLine($"Time: {t}");
+                }
+
+                Console.WriteLine("\n" + new string('=', 50) + "\n");
+                Console.WriteLine("SUMANDO t3 A CADA HORA:");
+                foreach (Time t in times)
+                {
+                    Console.WriteLine($"{t} + {t3} = {t.Add(t3)}");
+                }
+
+                Console.WriteLine("\n" + new string('=', 50) + "\n");
+                Console.WriteLine("¿PASA AL OTRO DÍA? (con t4):");
+                foreach (Time t in times)
+                {
+                    bool isOtherDay = t.IsOtherDay(t4);
+                    Console.WriteLine($"{t} + {t4} = {(isOtherDay ? "Sí" : "No")} pasa al otro día");
+                }
+
+                Console.ReadLine(); // Para que no se cierre la consola
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WorkshopPOO-01/WorkshopPOO-01.Backend/Time.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace WorkshopPOO_01.Backend
+{
+    public class Time
+    {
+        // Fields
+        private int _hour;
+        private int _minute;
+        private int _second;
+        private int _millisecond;
+
+        // Constructors
+        public Time() : this(0, 0, 0, 0) { }
+        public Time(int hour) : this(hour, 0, 0, 0) { }
+        public Time(int hour, int minute) : this(hour, minute, 0, 0) { }
+        public Time(int hour, int minute, int second) : this(hour, minute, second, 0) { }
+        public Time(int hour, int minute, int second, int millisecond)
+        {
+            _hour = ValidateHour(hour);
+            _minute = ValidateMinute(minute);
+            _second = ValidateSecond(second);
+            _millisecond = ValidateMillisecond(millisecond);
+        }
+
+        // Properties
+        public int Hour
+        {
+            get => _hour;
+            set => _hour = ValidateHour(value);
+        }
+
+        public int Minute
+        {
+            get => _minute;
+            set => _minute = ValidateMinute(value);
+        }
+
+        public int Second
+        {
+            get => _second;
+            set => _second = ValidateSecond(value);
+        }
+
+        public int Millisecond
+        {
+            get => _millisecond;
+            set => _millisecond = ValidateMillisecond(value);
+        }
+
+        // Methods
+        public long ToMilliseconds()
+        {
+            return _hour * 3600000L + _minute * 60000L + _second * 1000L + _millisecond;
+        }
+
+        public int ToSeconds()
+        {
+            return _hour * 3600 + _minute * 60 + _second;
+        }
+
+        public int ToMinutes()
+        {
+            return _hour * 60 + _minute;
+        }
+
+        public bool IsOtherDay(Time other)
+        {
+            return ToMilliseconds() + other.ToMilliseconds() >= 86400000;
+        }
+
+        public Time Add(Time other)
+        {
+            long totalMs = ToMilliseconds() + other.ToMilliseconds();
+
+            int newHour = (int)((totalMs / 3600000) % 24);
+            totalMs %= 3600000;
+
+            int newMinute = (int)(totalMs / 60000);
+            totalMs %= 60000;
+
+            int newSecond = (int)(totalMs / 1000);
+            int newMillisecond = (int)(totalMs % 1000);
+
+            return new Time(newHour, newMinute, newSecond, newMillisecond);
+        }
+
+        public override string ToString()
+        {
+            string amPm = _hour < 12 ? "AM" : "PM";
+            int hour12 = _hour % 12;
+            if (hour12 == 0) hour12 = 12;
+
+            return $"{hour12:D2}:{_minute:D2}:{_second:D2}.{_millisecond:D3} {amPm}";
+        }
+
+        // Validation Methods
+        private int ValidateHour(int hour)
+        {
+            if (hour < 0 || hour > 23)
+                throw new ArgumentOutOfRangeException(nameof(hour), "Las horas deben estar entre 0 y 23");
+            return hour;
+        }
+
+        private int ValidateMinute(int minute)
+        {
+            if (minute < 0 || minute > 59)
+                throw new ArgumentOutOfRangeException(nameof(minute), "Los minutos deben estar entre 0 y 59");
+            return minute;
+        }
+
+        private int ValidateSecond(int second)
+        {
+            if (second < 0 || second > 59)
+                throw new ArgumentOutOfRangeException(nameof(second), "Los segundos deben estar entre 0 y 59");
+            return second;
+        }
+
+        private int ValidateMillisecond(int millisecond)
+        {
+            if (millisecond < 0 || millisecond > 999)
+                throw new ArgumentOutOfRangeException(nameof(millisecond), "Los milisegundos deben estar entre 0 y 999");
+            return millisecond;
+        }
+    }
+}
